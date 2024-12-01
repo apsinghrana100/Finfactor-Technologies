25f43c6d2916a25feb004daa76e0a09b5a9b227d
Ajay Pratap Singh <116877279+apsinghrana100@users.noreply.github.com>
2024-12-01 21:58:28 +0530

    Weather system

---
+++ Backend/routes/weather.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getWeather } = require('../controller/weatherController');
+const router = express.Router();
+
+router.get('/:city', getWeather);
+
+module.exports = router;
